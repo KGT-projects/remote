1aef0fec62251442eb6c80cd90ca65f6e8db1e37
Kylian de Vlieg <150672020+KGT-projects@users.noreply.github.com>
2025-07-17 17:14:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const DEFAULT_DURATION = 7000; // Set initial countdown time in seconds
+const DEFAULT_DURATION = 200; // Set initial countdown time in seconds
 
 function getTimerData() {
   const stored = localStorage.getItem('remote-timer');
